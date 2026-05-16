b38d36f37ce923a996b81d55bbc8c82f0f59f0a2
luoxianye <231880344@smail.nju.edu.cn>
2026-05-17 00:20:55 +0800

    创建项目

---
+++ src/client/java/com/lxy/hook/client/HookModClient.java
@@ -0,0 +1,10 @@
+package com.lxy.hook.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HookModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
